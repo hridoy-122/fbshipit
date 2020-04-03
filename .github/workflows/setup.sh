103029694bcd2beca972621748c8bdfeeacac59d
Yunus Rahbar <yrahbar@fb.com>
2020-04-02 21:21:43 -0700

    Fix locale

--- .github/workflows/setup.sh
+++ .github/workflows/setup.sh
@@ -2,11 +2,6 @@
 set -ex
 
 add-apt-repository ppa:git-core/ppa
-# Must use a newer hg version than default on Xenial and lower
-UBUNTU_VERSION=$(lsb_release -r -s)
-if [ $(echo $UBUNTU_VERSION | cut -c1-2) -lt "18" ]; then
-  add-apt-repository ppa:mercurial-ppa/releases
-fi
 apt-get update
 apt-get install -y \
   git \
@@ -14,7 +9,7 @@ apt-get install -y \
   locales
 
 locale-gen en_US.UTF-8
-export LC_ALL=en_US.UTF-8
+echo "::set-env name=LC_ALL::en_US.UTF-8"
 
 git --version
 hg --version
